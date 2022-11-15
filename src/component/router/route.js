d3a0a87ed0e18c252721c28be3db6bf48e139727
念 <3209752489@qq.com>
2022-11-15 23:50:02 +0800

    设置路由根路径
    我试图在 github 页面上托管我的项目，项目在 localhost 中运行良好，但在 gh 页面上我得到空白页面，并且在控制台上出现类似这样的错误：
    
    Permissions-Policy 标头出错：未启用原始试验控制功能：'interest-cohort'。
    
    问题不是错误信息，是因为我使用的是react路由器，所以我需要根据github repo名称定义路由
    
    前：
    <Router>
      <Routes>
        <Route path="/" element={<Home />} />
      </Routes>
    </Router>
    
    后：
    <Router>
      <Routes>
        <Route path="/desa" element={<Home />} />
      </Routes>
    </Router>

--- src/component/router/route.js
+++ src/component/router/route.js
@@ -12,17 +12,20 @@ function MainRoute(props) {
     //长度为0 则匹配第一级路由；长度为二，则确定第一级路由，匹配第二级路由
     let path = props.path.concat();
     let route = routes.concat();
+    let rootPath = '/Web-Navigation-React';
     while (path.length > 0) {
         route = route[path.shift()] ?? [];
     }
     if (!route || route.length <= 0) {
         return;
     }
-    return (
-        <Routes>
-            {route.map(T => (<Route key={T.path} path={T.path} exact element={T.component}/>))}
-        </Routes>
-    );
+    route.forEach(T => T.path = rootPath + T.path);
+    function getRoute(item) {
+        return (<Route key={ item.path } path={ item.path } exact element={ item.component }/>);
+    }
+    return <Routes>
+        { route.map(getRoute) }
+    </Routes>;
 }
 
 export default MainRoute;
